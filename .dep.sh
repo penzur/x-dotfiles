86aaf312e92cb6a420e9280fe1852625dd94c6de
Elizar Pepino <jupenz@gmail.com>
2016-02-29 12:31:19 +0800

    Updates .dep.sh
    Signed-off-by: Elizar Pepino <jupenz@gmail.com>

--- .dep.sh
+++ .dep.sh
@@ -6,45 +6,31 @@ cd ~/.bin
 git clone https://github.com/rupa/z.git
 chmod +x ~/.bin/z/z.sh
 
-# janus vim
-cd ~/
-curl -Lo- https://bit.ly/janus-bootstrap | bash
+if [ ! -x $(which ruby) ]; then
+	echo 'Ruby not installed. Install ruby and re-run this script.'
+else
+	# janus vim
+	cd ~/
+	curl -Lo- https://bit.ly/janus-bootstrap | bash
 
-mkdir -p ~/.janus
-cd ~/.janus
+	mkdir -p ~/.janus
+	cd ~/.janus
 
-# Ack
-git clone https://github.com/mileszs/ack.vim.git
+	git clone https://github.com/mileszs/ack.vim.git
+	git clone https://github.com/digitaltoad/vim-jade.git
+	git clone https://github.com/mattn/emmet-vim.git
+	git clone https://github.com/michalliu/jsoncodecs.vim.git
+	git clone https://github.com/michalliu/sourcebeautify.vim.git
+	git clone https://github.com/michalliu/jsruntime.vim.git
+	git clone https://github.com/vim-scripts/Align.git
+	git clone https://github.com/tpope/vim-cucumber.git
 
-# Vim Powerline
-git clone https://github.com/Lokaltog/vim-powerline.git
+	git clone https://github.com/morhetz/gruvbox.git
+	git clone https://github.com/zeis/vim-kolor.git
+	git clone https://github.com/noahfrederick/vim-hemisu.git
+	git clone https://github.com/vim-scripts/TuttiColori-Colorscheme.git
 
-# vim-jade
-git clone https://github.com/digitaltoad/vim-jade.git
-
-# emmet-vim
-git clone https://github.com/mattn/emmet-vim.git
-
-# jsoncodecs
-git clone https://github.com/michalliu/jsoncodecs.vim.git
-
-# source-beautify
-git clone https://github.com/michalliu/sourcebeautify.vim.git
-
-# Align
-git clone https://github.com/vim-scripts/Align.git
-
-# jsruntime
-git clone https://github.com/michalliu/jsruntime.vim.git
-
-# gruvbox colorscheme
-git clone https://github.com/morhetz/gruvbox.git
-
-# kolors
-git clone https://github.com/zeis/vim-kolor.git
-
-# hemisu colorscheme
-git clone https://github.com/noahfrederick/vim-hemisu.git
-
-# tutticolori from Espresso app
-git clone https://github.com/vim-scripts/TuttiColori-Colorscheme.git
+	# Back to .dotfiles folder
+	cd ~/.dotfiles
+	source bootstrap.sh
+fi
