c8ad92f317cfbd29eaa94ce8ed40fb0755f7f8be
Elizar Pepino <jupenz@gmail.com>
2017-11-03 19:47:31 +0800

    Remove .dep.sh and add janus.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
+
+# Setup janus first
+source janus.sh
+
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
